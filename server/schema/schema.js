1a208772005156abbfb98edf76b638f67c710a64
金 鑫 <jinxin479@126com>
2020-03-03 00:07:36 +0800

    feat
    add author and book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,49 @@
 const graphql = require('graphql');
 const lodash = require('lodash');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
-var books=[
-    {name:"jixnin",genre:'fantash',id:'1'},
-    { name: "jixnin2", genre: 'fantash2', id: '2' },
-    { name: "jixnin3", genre: 'fantash3', id: '3' }
-]
+const { GraphQLObjectType, GraphQLString,GraphQLID,GraphQLInt,GraphQLList, GraphQLSchema } = graphql;
+var books = [
+  { name: "jixnin", genre: "fantash", id: "1", authorId: "1" },
+  { name: "jixnin1", genre: "fantash1", id: "4", authorId: "1" },
+  { name: "jixnin2", genre: "fantash2", id: "2", authorId: "2" },
+  { name: "jixnin3", genre: "fantash3", id: "3", authorId: "3" }
+];
+var authors = [
+  { name: "yuanshuzhi1", age: 30, id: "1" },
+  { name: "yuanshuzhi2", age: 31, id: "2" },
+  { name: "yuanshuzhi3", age: 32, id: "3" }
+];
 const BookType = new GraphQLObjectType({
-    name:'Book',
-    fields:()=>({
-        id: { type: GraphQLString},
-        name: { type: GraphQLString},
-        genre: { type: GraphQLString}
-    })
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author:{
+        type:AuthorType,
+        resolve(parent,args){
+            //parent指的是book，按照数据的层级关系确定
+           
+            return lodash.find(authors,{id:parent.authorId})
+        }
+    }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent,args){
+           console.log(1,parent);
+           console.log(books);
+          return lodash.filter(books, { authorId: parent.id });
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -26,6 +57,15 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args){
                 // get data from db
                 return lodash.find(books,{id:args.id});
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{
+                id:{type:GraphQLID}
+            },
+            resolve(parent,args){
+                return lodash.find(authors,{id:args.id})
             }
         }
     }
