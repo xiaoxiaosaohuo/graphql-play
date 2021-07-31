b90a6e82c27094c6243ec3e15dea86f4505fd686
金 鑫 <jinxin479@126com>
2021-07-31 16:13:19 +0800

    1
    1

--- server/app.js
+++ server/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require("graphql");
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const schema = require('./shcema/schema');
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
 
-// const schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
-// const root = { hello: (args) => {console.log(args);return 'Hello world!' }};
-
-
+// bind express with graphql
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    // rootValue: root,
-    graphiql: true,
+    schema,
+    graphiql: true
 }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
--- server/node_modules/graphql/execution/index.d.ts
+++ server/node_modules/graphql/execution/index.d.ts
@@ -2,12 +2,10 @@ export { pathToArray as responsePathAsArray } from '../jsutils/Path';
 
 export {
   execute,
-  executeSync,
   defaultFieldResolver,
   defaultTypeResolver,
   ExecutionArgs,
   ExecutionResult,
-  FormattedExecutionResult,
 } from './execute';
 
 export { getDirectiveValues } from './values';
